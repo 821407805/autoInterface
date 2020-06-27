9747107c657946e57a93b197fbd1795743faa9a1
jasonxiao <>
2020-06-27 19:35:31 +0800

    init autoInterface

---
+++ src/main/java/Test.java
@@ -0,0 +1,14 @@
+/**
+ * @Description: ddd
+ * @author Jasonxiao
+ * @date 2020/6/27 19:26
+ */
+
+/**
+ * @ClassName: Test
+ * @Description: ddd
+ * @Author Jason
+ * @Date 2020/6/27 19:26
+ */
+public class Test {
+}
